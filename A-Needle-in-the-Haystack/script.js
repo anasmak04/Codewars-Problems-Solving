947d6ef2fbc35ed49d3584a1b7f3dd829bde420b
anasmak04 <anasdev8@gmail.com>
2022-11-03 11:17:48 +0100

    resolve haystack prb

---
+++ A-Needle-in-the-Haystack/script.js
@@ -0,0 +1,8 @@
+// "found the needle at position " plus the index it found the needle, so:
+
+function findNeedle(haystack) {
+    let index = haystack.indexOf("needle")
+    for(let i of haystack)
+      return  i==="needle" ? `found the needle at position ${index}`
+  }
+  
