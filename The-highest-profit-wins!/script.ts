dbac4e5f2467d9632c95b2bc904ce79fefa0b462
anasmak04 <anasdev8@gmail.com>
2022-11-09 19:40:15 +0100

    add another prb with js ts

---
+++ The-highest-profit-wins!/script.ts
@@ -0,0 +1,14 @@
+function MinMax(arr : number[]) : number[]{
+    const List = [];
+      arr.sort(function(a,b) {return a - b})
+      let min  = arr[0];
+      let max = arr[arr.length - 1];
+  
+      List.push(min,max)
+  
+    return List;
+    
+  }
+  
+  let result = minMax([555,6,3])
+  console.log(result)
